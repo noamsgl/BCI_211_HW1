fd790ed8c5569fd51c7305947d4af0bb92d32f78
luvaton <evyatarluv@gmail.com>
2020-11-16 14:51:23 +0200

    Feature: Split sessions to windows

--- Evyatar/main.py
+++ Evyatar/main.py
@@ -8,8 +8,9 @@ import mne
 
 data_params = {
     'data_path': '..\\data\\dataset2',
-    'pickle_path': '..\\data',
-    'use_pickle': False
+    'pickle_path': {'X': '..\\data\\X.pickle',
+                    'y': '..\\data\\y.pickle'},
+    'use_pickle': True
 }
 
 filter_params = {
@@ -61,66 +62,75 @@ def get_labels(raw_labels):
     return current_labels
 
 
-def load_data(path, use_pickle, pickle_path):
+def load_sessions(path, use_pickle):
 
     """
-    create list of ndarray when each element is a window.
-    create list of labels for each window.
-    :param pickle_path: path to the pickle path
-    :param use_pickle: bool if to use the pickle file
+    create list of ndarray when each element is a session.
+    :param use_pickle: if to use the pickle file (bool)
     :param path: the path to the data directory
-    :return: two lists, one of windows (ndarray) and one of labels (str)
+    :return: list with all the sessions (ndarray)
     """
 
-    # Load the pickle file
-    if use_pickle:
-
-        return pickle.load(open(pickle_path + '\\windows.pickle', 'rb')),\
-               pickle.load(open(pickle_path + '\\labels.pickle', 'rb'))
+    # Verbose
+    print('Loading sessions...')
 
     # Load data
+    if use_pickle:
+        print('Using pickle file, not loading sessions')
+        return None
+
     records_name = generate_record_names()  # names of all the sessions
-    sessions = []  # list for all the sessions
-    labels = []  # list for all the labels
+    loaded_sessions = []  # list for all the loaded sessions
 
     for session in tqdm(records_name):
 
-        sessions.append(wfdb.io.rdrecord(path + '\\' + session).p_signal)  # get the full signal of the session
+        loaded_sessions.append(wfdb.io.rdrecord(path + '\\' + session).p_signal)  # get the full signal of the session
 
-        # window_index = wfdb.io.rdann(path + '\\' + session, 'win').sample  # get the indices of the windows
-
-        # windows += session_to_windows(p_signal, window_index)  # append the current session's windows
-
-        labels += get_labels(wfdb.io.rdann(path + '\\' + session, 'win').aux_note)
-        #     todo:
-    return sessions, labels
+    return loaded_sessions
 
 
-def preprocess_sessions(session, params):
+def preprocess_sessions(raw_session, params):
 
     """
 
-    :param params:
-    :param session: list with all the windows as ndarray
+    :param raw_session: list with all the sessions as ndarray
+    :param params: pre-processing params
     :return:
     """
 
+    # Verbose
+    print('\nPre-processing sessions...')
+
+    # Use pickle?
+    if raw_session is None:
+        print('Using pickle file, not filtering sessions')
+        return None
+
+    # Params
     bandpass = params['bandpass']
     notch = params['notch']
     fs = params['sample_rate']
 
-    session = session.T  # transpose the window
+    preprocessed_sessions = []
 
-    # Normalization (mean = 0, std = 1)
-    session = np.nan_to_num((session - session.mean(axis=0)) / session.std(axis=0))
+    for s in tqdm(raw_session):
 
-    # Bandpass filter
-    session = butter_bandpass_filter(session, bandpass[0], bandpass[1], fs)
-    
-    # Notch filter
-    mne.filter.notch_filter(session, fs, notch)
+        # transpose the session
+        s = s.T
 
-    return session.T
+        # Normalization (mean = 0, std = 1)
+        s = np.nan_to_num((s - s.mean(axis=0)) / s.std(axis=0))
+
+        # Bandpass filter
+        s = butter_bandpass_filter(s, bandpass[0], bandpass[1], fs)
+
+        # Notch filter
+        s = mne.filter.notch_filter(s, fs, notch, verbose=False)
+
+        # Add to the preprocessed list
+        preprocessed_sessions.append(s.T)
+
+    return preprocessed_sessions
 
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
@@ -139,13 +149,54 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     return y
 
 
+def split_to_windows(sessions, path, pickle_path):
+
+    """
+    Split the pre-processed sessions into windows.
+    The function also return list of the labels
+    :param pickle_path: path to the pickle file
+    :param path: path to the data folder
+    :param sessions: list with all the sessions
+    :return: list to the windows (ndarray) and list of labels (str)
+    """
+
+    # Verbose
+    print('\nSplitting to windows...')
+
+    # Use pickle?
+    if sessions is None:
+        print('Loading windows from pickle file')
+        return pickle.load(open(pickle_path['X'], 'rb')), pickle.load(open(pickle_path['y'], 'rb'))
+
+    records_name = generate_record_names()  # names of all the sessions
+    windows = []
+    labels = []
+
+    for i, session in enumerate(records_name):
+
+        window_index = wfdb.io.rdann(path + '\\' + session, 'win').sample  # get the indices of the windows
+
+        windows += session_to_windows(sessions[i], window_index)  # append the current session's windows
+
+        labels += get_labels(wfdb.io.rdann(path + '\\' + session, 'win').aux_note)
+
+    return windows, labels
+
+
 if __name__ == '__main__':
 
-    sessions, labels = load_data(path=data_params['data_path'],
-                                use_pickle=data_params['use_pickle'],
-                                pickle_path=data_params['pickle_path'])
+    # Get the sessions of the data
+    sessions = load_sessions(path=data_params['data_path'],
+                             use_pickle=data_params['use_pickle'])
 
-    sessions = [preprocess_sessions(s, filter_params) for s in sessions]
+    # Filter the sessions
+    sessions = preprocess_sessions(raw_session=sessions,
+                                   params=filter_params)
+
+    # Split to windows
+    X, y = split_to_windows(sessions=sessions,
+                            path=data_params['data_path'],
+                            pickle_path=data_params['pickle_path'])
 
 
 
